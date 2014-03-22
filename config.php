f2e213fd1c347b17dc9eccc72a5ea6613e53cbc4
Thibaud Colas <thibaudcolas@gmail.com>
2014-03-22 01:15:19 +0100

    Remove short URL

--- config.php
+++ config.php
@@ -5,8 +5,6 @@ define('MYSQL_USER', 'user');
 define('MYSQL_PASSWORD', 'password');
 define('MYSQL_DATABASE', 'database');
 define('TWITTER_USERNAME', 'ThibWeb');
-// Include the trailing slash !
-define('SHORT_URL', 'http://tc.fr/');
 // Ommit the trailing slash !
 define('DEFAULT_URL', 'http://thibaudcolas.fr');
 
