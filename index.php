9f5b2601651444cdc6b30dab860ef8396c0a5274
Thibaud Colas <thibaudcolas@gmail.com>
2014-11-07 00:26:39 +0100

    Switch to HTTPS for Twitter & xkcd

--- index.php
+++ index.php
@@ -13,43 +13,43 @@ $url = DEFAULT_URL . '/';
 
 if (isset($_GET['slug'])) {
 
-	$slug = $_GET['slug'];
+    $slug = $_GET['slug'];
 
-	if ($slug == '@') {
-		$url = 'http://twitter.com/' . TWITTER_USERNAME;
-	} else {
-		// Once @ is checked, all special chars are removed.
-		$slug = preg_replace('/[^a-z0-9]/si', '', $slug);
+    if ($slug == '@') {
+        $url = 'https://twitter.com/' . TWITTER_USERNAME;
+    } else {
+        // Once @ is checked, all special chars are removed.
+        $slug = preg_replace('/[^a-z0-9]/si', '', $slug);
 
-		// Less than eight : Slug is xkcd, greater : Tweet.
-		if (is_numeric($slug) && strlen($slug) > 0) {
-			if (strlen($slug) < 5) {
-				$url = 'http://xkcd.com/' . $slug;
-			}
-			else if (strlen($slug) > 8) {
-				$url = 'http://twitter.com/' . TWITTER_USERNAME . '/status/' . $slug;
-			}
-			else {
-				$url = DEFAULT_URL;
-			}
-		} else {
+        // Less than eight : Slug is xkcd, greater : Tweet.
+        if (is_numeric($slug) && strlen($slug) > 0) {
+            if (strlen($slug) < 5) {
+                $url = 'https://xkcd.com/' . $slug;
+            }
+            else if (strlen($slug) > 8) {
+                $url = 'https://twitter.com/' . TWITTER_USERNAME . '/status/' . $slug;
+            }
+            else {
+                $url = DEFAULT_URL;
+            }
+        } else {
 
-			$db = new MySQLi(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE);
-			$db->set_charset('utf8');
+            $db = new MySQLi(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE);
+            $db->set_charset('utf8');
 
-			$escapedSlug = $db->real_escape_string($slug);
-			$redirectResult = $db->query('SELECT url FROM redirect WHERE slug = "' . $escapedSlug . '"');
+            $escapedSlug = $db->real_escape_string($slug);
+            $redirectResult = $db->query('SELECT url FROM redirect WHERE slug = "' . $escapedSlug . '"');
 
-			if ($redirectResult && $redirectResult->num_rows > 0) {
-				$db->query('UPDATE redirect SET hits = hits + 1 WHERE slug = "' . $escapedSlug . '"');
-				$url = $redirectResult->fetch_object()->url;
-			} else {
-				$url = DEFAULT_URL . $_SERVER['REQUEST_URI'];
-			}
+            if ($redirectResult && $redirectResult->num_rows > 0) {
+                $db->query('UPDATE redirect SET hits = hits + 1 WHERE slug = "' . $escapedSlug . '"');
+                $url = $redirectResult->fetch_object()->url;
+            } else {
+                $url = DEFAULT_URL . $_SERVER['REQUEST_URI'];
+            }
 
-			$db->close();
-		}
-	}
+            $db->close();
+        }
+    }
 }
 
 header('Location: ' . $url, null, 301);
