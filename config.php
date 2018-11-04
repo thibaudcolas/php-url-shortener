ad7b1c52a41845171f03f655d4fded6fd036ce50
Thibaud Colas <thibaudcolas@gmail.com>
2018-11-05 00:11:37 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('MYSQL_HOST', 'localhost');
 define('MYSQL_USER', 'url-shortener');
 define('MYSQL_PASSWORD', 'password');
 define('MYSQL_DATABASE', 'url-shortener');
-define('TWITTER_USERNAME', 'ThibWeb');
+define('TWITTER_USERNAME', 'thibaud_colas');
 // Ommit the trailing slash !
 define('DEFAULT_URL', 'https://thib.me');
 
